b90fd34ff1191ee3bf77207b991c8c464ac6babf
Maximilian Falco Widjaya <z5373431@ad.unsw.edu.au>
2023-07-13 15:11:34 +1000

    Added new code

--- main.r
+++ main.r
@@ -1,6 +1,8 @@
 library(readr)
 library(dplyr)
 library(moments)
+library(rpart)
+library(rpart.plot)
 
 data <- read.csv("https://raw.githubusercontent.com/dat-analytics/data_access_2_t2_2023/main/z5373431_z5373431-Assessment2Data.csv", encoding="UTF-8")
 data
@@ -10,6 +12,10 @@ trimmedData
 #########################################################################################################################
 ## Made data visualizations on each column, the C_Gender one is kind of the most complicated but can act as a template ##
 ## if we want to make it look better for the final output                                                              ##
+#########################################################################################################################
+
+#########################################################################################################################
+## Descriptive Analysis                                                                                                ##
 #########################################################################################################################
 
 # Total Spend
@@ -28,10 +34,18 @@ title(main="Gender", cex.main=1.5)
 boxplot(trimmedData$C_Age)
 min(trimmedData$C_Age)
 max(trimmedData$C_Age)
-# Might want to make a grouped bar chart for this one
 mean(trimmedData$C_Age)
 median(trimmedData$C_Age)
 kurtosis(trimmedData$C_Age)
+
+age_10s <- nrow(trimmedData[trimmedData$C_Age > 9 & trimmedData$C_Age < 20,])
+age_20s <- nrow(trimmedData[trimmedData$C_Age > 19 & trimmedData$C_Age < 30,])
+age_30s <- nrow(trimmedData[trimmedData$C_Age > 29 & trimmedData$C_Age < 40,])
+age_40s <- nrow(trimmedData[trimmedData$C_Age > 39 & trimmedData$C_Age < 50,])
+age_50s <- nrow(trimmedData[trimmedData$C_Age > 49 & trimmedData$C_Age < 60,])
+age_60s <- nrow(trimmedData[trimmedData$C_Age > 59 & trimmedData$C_Age < 70,])
+
+barplot(c(age_10s,age_20s,age_30s,age_40s,age_50s,age_60s), xlab = "Age", ylim = c(0,1200), names.arg = c("10s","20s","30s","40s","50s","60s"))
 
 # State
 states <- table(trimmedData$C_State)
@@ -79,3 +93,48 @@ satisfaction <- replace(satisfaction, satisfaction == "L", 0)
 # 1 here represents 'H' and 0 represents 'L'
 spending <- trimmedData$C_TotalSpend
 boxplot(spending ~ satisfaction)
+satisfaction <- as.numeric(satisfaction)
+cor(satisfaction, spending)
+
+# Box plot of Customer Satisfaction and Referrals
+referral <- trimmedData$App_Referral
+boxplot(referral ~ satisfaction)
+referralSatisfaction <- trimmedData[, c('App_SatisfactionRating','App_Referral')]
+referralSatisfied <- referralSatisfaction[referralSatisfaction$App_SatisfactionRating == "H",]
+referralNotSatisfied <- referralSatisfaction[referralSatisfaction$App_SatisfactionRating == "L",]
+
+mean(referralSatisfied$App_Referral)
+mean(referralNotSatisfied$App_Referral)
+# Data suggests that with high satisfaction ratings, customers are more likely to refer ASAL's products and app
+
+# Box plot of Referrals and Promotion
+yesPromotion <- trimmedData[trimmedData$App_Promotion == "Yes",]
+noPromotion <- trimmedData[trimmedData$App_Promotion == "No",]
+
+summary(yesPromotion)
+table(yesPromotion$App_SatisfactionRating)
+summary(noPromotion)
+table(noPromotion$App_SatisfactionRating)
+
+promotion <- trimmedData$App_Promotion
+promotion <- replace(promotion, promotion == "Yes", 1)
+promotion <- replace(promotion, promotion == "No", 0)
+promotion <- as.numeric(promotion)
+cor(promotion, spending)
+
+#########################################################################################################################
+## Predictive Analysis                                                                                                 ##
+#########################################################################################################################
+
+trimmedData
+set.seed(1)
+# 70/30 train test split
+split <- 0.70*2000
+train <- sample(2000, split)
+trimmedDataTrain <- trimmedData[train,]
+trimmedDataTest <- trimmedData[-train,]
+
+tree <- rpart(App_SatisfactionRating ~ App_Referral + App_Tenure, data = trimmedDataTrain)
+rpart.plot(tree, yesno = 2)
+tree <- rpart(App_SatisfactionRating ~ C_Age + App_Referral, data = trimmedDataTrain)
+rpart.plot(tree, yesno = 2)
